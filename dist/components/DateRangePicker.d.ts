916e84a5eeeaf8be92020939a76806c5d58f1b07
Aashik Hameed A J <>
2023-01-26 19:08:08 +0530

    initial commit

---
+++ dist/components/DateRangePicker.d.ts
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { DateRange, DefinedRange } from '../types';
+declare type Marker = symbol;
+export declare const MARKERS: {
+    [key: string]: Marker;
+};
+interface DateRangePickerProps {
+    open: boolean;
+    initialDateRange?: DateRange;
+    definedRanges?: DefinedRange[];
+    minDate?: Date | string;
+    maxDate?: Date | string;
+    showHeader: boolean;
+    onChange: (dateRange: DateRange) => void;
+}
+declare const DateRangePicker: React.FunctionComponent<DateRangePickerProps>;
+export default DateRangePicker;
